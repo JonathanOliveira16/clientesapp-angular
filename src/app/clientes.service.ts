167198b6f478246ef31905a568d394cda10640a0
JonathanOliveira16 <65185717+JonathanOliveira16@users.noreply.github.com>
2021-03-11 12:35:51 -0300

    commit inicial

---
+++ src/app/clientes.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Cliente } from './clientes/cliente';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientesService {
+
+  constructor(private http : HttpClient) { 
+    
+  }
+
+  getCliente() : Cliente{
+    let cliente : Cliente = new Cliente();
+    cliente.nome = 'aaa';
+    cliente.cpf = '12';
+    return cliente  
+  }
+
+  salvar(cliente : Cliente) : Observable<Cliente> {
+    return this.http.post<Cliente>('http://localhost:8080/api/clientes',cliente)
+  }
+}
